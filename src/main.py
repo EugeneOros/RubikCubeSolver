8272a7e78196259c9f7be009f8c132616af0a568
Patrycja Motak <patrycjamotak@macbook-air-patrycja.home>
2021-04-29 13:47:35 +0200

    bad scramble error message

--- src/main.py
+++ src/main.py
@@ -12,6 +12,8 @@ def error(state):
         print('\033[0;33m[error] Cube has already been solved')
     elif state == Errors.STOP:
         print('\033[0;33m[error] Program stopped')
+    elif state == Errors.INCORRECTLY_SCRAMBLED:
+        print('\033[0;33m[error] The scanned cube does not match with generated scramble (to toggle scramble mode press \"s\"). \n Make sure you scanned the cube correctly')
     sys.exit(state)
 
 
